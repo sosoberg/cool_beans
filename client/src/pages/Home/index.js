917d838b03d59da5d6eec237b332181784b3a7a6
Sam Oberg <samueloberg@hotmail.com>
2021-08-30 10:15:29 -0700

    home page styling

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 
+import './style.css'
+
 function Home() {
   return (
-    <div>
-        <p>Home</p>
-    </div>
-    
+    <>
+      <div>
+          <p>Home</p>
+      </div>
+      <div className='homeImageGridOne'>
+        <div className='cropped'>
+          <img src='../assets/images/coffeeImage.jpg' alt='pouring coffee'/>
+        </div>
+        <div className='cropped'>
+          <img className='contactImage' src='../assets/images/contactImage.jpg' alt='contact'/>
+        </div>
+      </div>
+      <div className='cropped2'>
+          <img src='../assets/images/matterhorn.jpg' alt='matterhorn'/>
+      </div>
+    </>
   );
 }
 
