7877f80554b184c3e2f769dbee7674d83f5aaf18
Sam Oberg <samueloberg@hotmail.com>
2021-08-30 10:43:57 -0700

    home page responsive work

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -10,13 +10,16 @@ function Home() {
       </div>
       <div className='homeImageGridOne'>
         <div className='cropped'>
+          <p className='pictureTitle'>Menu</p>
           <img src='../assets/images/coffeeImage.jpg' alt='pouring coffee'/>
         </div>
         <div className='cropped'>
+          <p className='pictureTitle'>Contact</p>
           <img className='contactImage' src='../assets/images/contactImage.jpg' alt='contact'/>
         </div>
       </div>
       <div className='cropped2'>
+          <p className='pictureTitle2'>Explore</p>
           <img src='../assets/images/matterhorn.jpg' alt='matterhorn'/>
       </div>
     </>
